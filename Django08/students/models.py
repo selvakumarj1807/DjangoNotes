eaba02218bfefc2daacfc75f4c4a41b8294586a4
selvakumarj1807 <selvakumarj1807@gmail.com>
2025-05-20 10:29:20 +0530

    api testing

--- Django08/students/models.py
+++ Django08/students/models.py
@@ -4,6 +4,7 @@ class Student(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     course = models.CharField(max_length=100)
+    mobile = models.CharField(max_length=15)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
